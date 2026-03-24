ce1831d82330f7262fe6b5d339b08b7f509769b7
nnpsvulul-cpu <nnpsvulul@gmail.com>
2026-03-24 10:18:43 +0300

    Добавленн класс Dog

--- LB1/Program.cs
+++ LB1/Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Variant10
+{
+    // Класс Собака
+    class Dog
+    {
+        private string nickname;
+        private string breed;
+        private float age;
+
+        public Dog()
+        {
+            nickname = "Неизвестно";
+            breed = "Неизвестно";
+            age = 0f;
+        }
+
+        public Dog(string nickname, string breed, float age)
+        {
+            this.nickname = nickname;
+            this.breed = breed;
+            this.age = age;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Кличка: {nickname}");
+            Console.WriteLine($"Порода: {breed}");
+            Console.WriteLine($"Возраст: {age} лет");
+            Console.WriteLine(new string('-', 30));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
