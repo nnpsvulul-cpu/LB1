7d65d6e1a07c0617244b71e49c1d3926b2889f79
nnpsvulul-cpu <nnpsvulul@gmail.com>
2026-03-24 10:20:29 +0300

    Реализация Main

--- LB1/Program.cs
+++ LB1/Program.cs
@@ -36,7 +36,22 @@ namespace Variant10
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Демонстрация класса Dog");
+            Console.WriteLine();
 
+            Dog dog1 = new Dog();
+            Console.WriteLine("Объект dog1 (конструктор по умолчанию):");
+            dog1.Print();
+
+            Dog dog2 = new Dog("Рекс", "Немецкая овчарка", 3.5f);
+            Console.WriteLine("Объект dog2 (конструктор с параметрами):");
+            dog2.Print();
+
+            Dog dog3 = new Dog("Шарик", "Дворняга", 1.2f);
+            Console.WriteLine("Объект dog3:");
+            dog3.Print();
+
+            Console.ReadKey();
         }
     }
 }
